a09e0889b5a6463e929a0e2ec68af246d5668fd1
Juho Niinikoski <juho.niinikoski@aalto.fi>
2021-07-08 17:56:24 +0300

    typescript config to server

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.log('toimii')
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
